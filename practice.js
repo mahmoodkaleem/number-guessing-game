604d9f230117ccfbf7b4f9ea400c1a6cab12760b
mahmoodkaleem <doc.mmkaleem@gmail.com>
2022-08-31 18:00:03 +0530

    initial commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+// const li = document.getElementsByTagName('li')
+// console.log(li)
+
+// for(var i = 0; i<li.length; i++){
+//     if(i%2==0){
+//         li[i].style.color = "blue"
+//     }else{
+//         li[i].style.color= "green"
+//     }
+// }
+
+let a = Math.floor(Math.random()*10);
+    console.log(a)
